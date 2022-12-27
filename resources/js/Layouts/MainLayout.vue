0cbe7a6ebe89cff718073f6bd5b974aa16b0ed6c
kamrulcse404 <kamrul.h01960@gmail.com>
2022-12-27 21:37:32 +0600

    getting the currently logged in user

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,36 +1,47 @@
 <template>
-    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+    <header
+        class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full"
+    >
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
                     <Link :href="route('listing.index')">Listings</Link>
                 </div>
-                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                <div
+                    class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center"
+                >
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
-                <div>
-                    <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+                <div class="flex items-center gap-4" v-if="user">
+                    <div class="text-sm text-gray-500">{{ user.name }}</div>
+                    <Link :href="route('listing.create')" class="btn-primary"
+                        >+ New Listing</Link
+                    >
+                    <div>Logout</div>
+                </div>
+                <div v-else>
+                    <Link :href="route('login')">Sign-In</Link>
                 </div>
             </nav>
         </div>
     </header>
 
     <main class="container mx-auto p-4 w-full">
-        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
-        {{ flashSuccess }}
-    </div>
-    <slot>Default</slot>
+        <div
+            v-if="flashSuccess"
+            class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
+        >
+            {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
     </main>
-
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { Link, usePage } from '@inertiajs/inertia-vue3'
+import { computed } from "vue";
+import { Link, usePage } from "@inertiajs/inertia-vue3";
 
-const page = usePage()
-const flashSuccess = computed(() => page.props.value.flash.success )
-
+const page = usePage();
+const flashSuccess = computed(() => page.props.value.flash.success);
+const user = computed(() => page.props.value.user);
 </script>
-
-
