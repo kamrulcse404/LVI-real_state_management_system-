2b0eb2a2496bc8b7cd617cad20ef7b64f0b5aa38
kamrulcse404 <kamrul.h01960@gmail.com>
2022-12-28 02:40:26 +0600

    form links

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,30 +3,53 @@
         <div class="w-1/2 mx-auto">
             <div>
                 <label for="email" class="label">E-mail</label>
-                <input id="email" type="text" class="input" v-model="form.email" />
-                <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
+                <input
+                    id="email"
+                    type="text"
+                    class="input"
+                    v-model="form.email"
+                />
+                <div v-if="form.errors.email" class="input-error">
+                    {{ form.errors.email }}
+                </div>
             </div>
-            
+
             <div class="mt-4">
                 <label for="password" class="label">Password</label>
-                <input id="password" type="password" class="input" v-model="form.password" />
-                <div v-if="form.errors.password" class="input-error">{{ form.errors.password }}</div>
+                <input
+                    id="password"
+                    type="password"
+                    class="input"
+                    v-model="form.password"
+                />
+                <div v-if="form.errors.password" class="input-error">
+                    {{ form.errors.password }}
+                </div>
             </div>
-            
+
             <div class="mt-4">
                 <button class="btn-primary w-full" type="submit">Login</button>
+
+                <div class="mt-2 text-center">
+                    <Link
+                        :href="route('user-account.create')"
+                        class="text-sm text-gray-500"
+                    >
+                        Need an account? Click here
+                    </Link>
+                </div>
             </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/inertia-vue3';
+import { useForm, Link } from '@inertiajs/inertia-vue3'
 
 const form = useForm({
     email: null,
-    password: null
-})
+    password: null,
+});
 
-const login = () => form.post(route('login.store'))
+const login = () => form.post(route("login.store"));
 </script>
