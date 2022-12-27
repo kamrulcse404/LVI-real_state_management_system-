95302948076de5898db13301c9c29f702411a99e
kamrulcse404 <kamrul.h01960@gmail.com>
2022-12-28 02:12:32 +0600

    user registration logic & hashing password

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Auth;
+use Hash;
 use Illuminate\Http\Request;
 
 class UserAccountController extends Controller
@@ -9,5 +12,21 @@ class UserAccountController extends Controller
     public function create()
     {
         return inertia('UserAccount/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::make($request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]));
+
+        $user->password = Hash::make($user->password);
+        $user->save();
+        Auth::login($user);
+
+        return redirect()->route('listing.index')
+        ->with('success', 'Account created!');
     }
 }
